33c76157976abfe43efa27db294fb28e5051a5f3
Alan Lopez Cruz <alanestudio2206@gmail.com>
2026-09-04 08:03:42 -0600

    Inicializa proyecto y documento de análisis

--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
-public class main {
+public class main
+{
+    public class Main
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("Sistema de monitoreo");
+        }
+    }
 }
