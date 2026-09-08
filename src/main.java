015d79eca6027a37d645a047b63bc7b7e04a7be2
Alan Lopez Cruz <alanestudio2206@gmail.com>
2026-09-07 19:56:04 -0600

    Aregla problema de compilacion

--- src/main.java
+++ src/main.java
@@ -1,10 +1,6 @@
-public class main
-{
-    public class Main
-    {
+public class main {
         public static void main(String[] args)
         {
             System.out.println("Sistema de monitoreo");
         }
-    }
 }
