7e6c14dc74686eb0c5a3822aead52d4aaadb528d
ianro <irodriguez45@alumnos.uaq.mx>
2026-09-17 09:23:33 -0600

    Se intenta subir la imagen uml-inicial.png

--- src/main.java
+++ src/main.java
@@ -4,3 +4,26 @@ public class main {
             System.out.println("Sistema de monitoreo");
         }
 }
+
+enum EstadoTanque{
+    llenado, vaciando, detenido;
+}
+public final class Tanque {
+    private String Id;
+    private double capacidadMaxima;
+    private double nivelActual;
+    private EstadoTanque estado;
+    private Tanque() {
+    }
+
+    static Tanque createTanque() {
+        return new Tanque();
+    }
+}
+
+class SensorNivel {
+    private String id;
+    private double lastLEcture;
+    private Tanque tanqueAsociado;
+
+}
